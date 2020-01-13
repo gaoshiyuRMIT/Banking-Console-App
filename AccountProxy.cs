b174c42d8d12873c0d7c8e0e699b598bb10ebba7
rmit-s3553348-louis-abasta <s3553348@student.rmit.edu.au>
2020-01-13 15:43:20 +1100

    Add files via upload
    Inital AccountProxy

---
+++ AccountProxy.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace Banking.DBManager
+{
+
+    public class AccountProxy : AccountManager
+    {
+        public AccountProxy(string connS) : base(connS)
+        {
+
+        }
+
+       
+
+        public interface IAcountManager
+
+        {
+            public void Deposit(int accNo, double amount, string comment);
+
+            public void WithDraw(int accNo, double amount, string comment);
+
+            public void Transfer(int srcNo, int destNo, double amount,
+                string comment);
+
+        }
+
+        class Account : IAcountManager
+        {
+            private int IAccNo;
+            private double IAmount;
+            private string IComment;
+
+            public void Deposit(int accNo, double amount, string comment)
+            {
+
+                IAccNo = accNo;
+                IAmount = amount;
+                IComment = comment;
+
+                return;
+
+            }
+
+            public void Transfer(int srcNo, int destNo, double amount, string comment)
+            {
+
+            }
+
+            public void WithDraw(int accNo, double amount, string comment)
+            {
+
+
+            }
+        }
+
+        class AccountManagerProxy : IAcountManager
+        {
+            private Account _account = new Account();
+
+            public void Deposit(int accNo, double amount, string comment)
+            {
+                ((IAcountManager)_account).Deposit(accNo, amount, comment);
+            }
+
+            public void Transfer(int srcNo, int destNo, double amount, string comment)
+            {
+                ((IAcountManager)_account).Transfer(srcNo, destNo, amount, comment);
+            }
+
+            public void WithDraw(int accNo, double amount, string comment)
+            {
+                ((IAcountManager)_account).WithDraw(accNo, amount, comment);
+            }
+        }
+
+    }
+
+}
+
+
+   
+
+    
+
