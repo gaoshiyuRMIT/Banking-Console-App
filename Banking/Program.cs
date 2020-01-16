0410857c83f430354919518d407092c4e2a64281
Shiyu <s3734720@student.rmit.edu.au>
2020-01-16 15:15:33 +1100

    feature: UI

---
+++ Banking/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Banking.UINS;
+
+namespace Banking
+{
+    public class Program
+    {
+        public Program()
+        {
+            
+        }
+        public static void Main()
+        {
+            new UI().Index();
+        }
+    }
+}
